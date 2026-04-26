729abd861f1b42e0da64043d639f6704fda8944c
Everton-alt <evertoncl2000@gmail.com>
2026-04-26 02:50:43 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 const authRoutes = require('./routes/auth');
 const palpitesRoutes = require('./routes/palpites');
 const importacaoRoutes = require('./routes/importacao');
+const usuariosRoutes = require('./routes/usuarios');
+const vitoriasRoutes = require('./routes/vitorias');
+const noticiasRoutes = require('./routes/noticias');
 
 const app = express();
 
@@ -20,15 +24,17 @@ app.use(cors({
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
 
-// SERVIR SITE
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/palpites', palpitesRoutes);
 app.use('/api/importacao', importacaoRoutes);
+app.use('/api/usuarios', usuariosRoutes);
+app.use('/api/vitorias', vitoriasRoutes);
+app.use('/api/noticias', noticiasRoutes);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/login.html');
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
 const PORT = process.env.PORT || 3000;
