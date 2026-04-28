6a193ee42c9418d114ed873599ca2c66dbb4964a
Everton-alt <evertoncl2000@gmail.com>
2026-04-28 19:41:01 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+// index.php (Raiz)
+// Este arquivo garante que qualquer pessoa que acesse a URL principal 
+// seja mandada para a tela de login correta.
+
+header("Location: public/login.php");
+exit();
+?>
