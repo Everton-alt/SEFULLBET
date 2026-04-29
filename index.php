5d17e1dee8afeec7df6a5b7585787b0a029cd311
Everton-alt <evertoncl2000@gmail.com>
2026-04-29 08:43:28 -0300

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-// index.php (Raiz)
-// Este arquivo garante que qualquer pessoa que acesse a URL principal 
-// seja mandada para a tela de login correta.
-
-header("Location: login.php");
-exit();
-?>
