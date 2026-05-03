67fecf0f76892fe1252afd9823edbb01dbaaf1f1
Everton-alt <evertoncl2000@gmail.com>
2026-05-03 20:39:27 -0300

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -197,7 +197,7 @@ $stat_v = getStats($pdo, 'VIP');
         <div style="font-size: 11px; font-weight: 700; display: flex; flex-direction: column; align-items: flex-end;">
             <span style="color: <?= $cor_perfil ?>; text-transform: uppercase;">
                 <i class="fa-solid fa-crown" style="font-size: 9px;"></i> 
-                <?= htmlspecialchars($plano_interesse) ?>
+                <?= htmlspecialchars($plano_pre_selecionado) ?>
             </span>
             <span style="color: var(--text-dim);">
                 Créditos: <b id="header-saldo" style="color: var(--primary);">
